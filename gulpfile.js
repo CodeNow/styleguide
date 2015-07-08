05093cdce03cd22411e0779007ce18a27ef12125
runnabro <tli415@gmail.com>
2015-07-08 12:25:42 -0700

    add sass task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('styles', function() {
+  gulp.src('styles/index.scss')
+    .pipe(sass({
+        errLogToConsole: true
+    }))
+    .pipe(gulp.dest('./styles/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('styles/**/*.scss',['styles']);
 });
