453a07abc4262e15fe61eedc58afe58abd19f919
runnabro <tli415@gmail.com>
2015-07-08 14:31:15 -0700

    setup html templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
+
+gulp.task('fileinclude', function() {
+  gulp.src(['html/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('styles', function() {
   gulp.src('styles/index.scss')
@@ -12,4 +22,5 @@ gulp.task('styles', function() {
 //Watch task
 gulp.task('default',function() {
     gulp.watch('styles/**/*.scss',['styles']);
+    gulp.watch('html/*.html', ['fileinclude']);
 });
