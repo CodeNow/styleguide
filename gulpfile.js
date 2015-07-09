91cc80469c6abe41268821510275e0e4ebb7596a
runnabro <tli415@gmail.com>
2015-07-08 21:28:06 -0700

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fileinclude = require('gulp-file-include'); // html
 var sass = require('gulp-sass'); // sass
+var autoprefixer = require('gulp-autoprefixer'); // autoprefixer
 
 gulp.task('fileinclude', function() {
   gulp.src(['html/index.html'])
@@ -8,21 +9,28 @@ gulp.task('fileinclude', function() {
       prefix: '@@',
       basepath: '@file'
     }))
-    .on('error', function(err){ console.log(err.message); }) // on error
+    .on('error', function(err){
+      console.log(err.message);
+    })
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('styles', function() {
+gulp.task('sass', function() {
   gulp.src('styles/index.scss')
     .pipe(sass({
         errLogToConsole: true
     }))
-    .on('error', function(err){ console.log(err.message); }) // on error
+    .on('error', function(err){
+      console.log(err.message);
+    })
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
     .pipe(gulp.dest('./styles/'));
 });
 
-//Watch task
-gulp.task('default', ['fileinclude', 'styles'], function() {
-    gulp.watch('styles/**/*.scss',['styles']);
+// watch task
+gulp.task('default', ['fileinclude', 'sass'], function() {
+    gulp.watch('styles/**/*.scss', ['sass']);
     gulp.watch('html/*.html', ['fileinclude']);
 });
